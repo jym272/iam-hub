33f42e8189c95ee7cdaba09b15f2ab3bc0a46e25
Jorge Clavijo <jorge.clavijo@gm2dev.com>
2025-09-30 14:45:40 -0300

    feat: regions per user (#2)
    Co-authored-by: jym272 <jym272@users.noreply.github.com>

--- src/iam/groups/index.ts
+++ src/iam/groups/index.ts
@@ -3,4 +3,4 @@ export * from "./billing.ts";
 export * from "./groups.ts";
 export * from "./iam-restriction.ts";
 export * from "./instance-restriction.ts";
-export * from "./region-restriction.ts";
+export { createUserRegionRestrictionGroup } from "./region-restriction.ts";
